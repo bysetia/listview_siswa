2fe078b9df4400554e38bde48625cf5688b60e1e
bysetia <bayuseptiankurniawan@gmail.com>
2022-08-11 17:13:23 +0700

    style: [BK] add tabBar in Navbar

--- belajar_listview/lib/list_siswa.dart
+++ belajar_listview/lib/list_siswa.dart
@@ -14,200 +14,167 @@ class _ListSiswaState extends State<ListSiswa> {
         kelas: 'Kelas: 11 PPLG 1 || ',
         absen: "Absen: 01",
         image: 'image/pp.jpeg'),
-
-        User(
+    User(
         name: 'Ahmad Aziz Wira Widodo',
         kelas: 'Kelas: 11 PPLG 1 || ',
         absen: "Absen: 02",
         image: 'image/pp.jpeg'),
-
-        User(
+    User(
         name: 'Akbar Risqullah Putra Susanto',
         kelas: 'Kelas: 11 PPLG 1 || ',
         absen: "Absen: 03",
         image: 'image/pp.jpeg'),
-
-        User(
+    User(
         name: 'Alwan Athallah Mumtaz',
         kelas: 'Kelas: 11 PPLG 1 || ',
         absen: "Absen: 04",
         image: 'image/pp.jpeg'),
-
-        User(
+    User(
         name: 'Amri iqro Samudra Al-Fatihah',
         kelas: 'Kelas: 11 PPLG 1 || ',
         absen: "Absen: 05",
         image: 'image/pp.jpeg'),
-
-        User(
+    User(
         name: 'Anakku Lanang Sejati Adli',
         kelas: 'Kelas: 11 PPLG 1 || ',
         absen: "Absen: 06",
         image: 'image/pp.jpeg'),
-
-        User(
+    User(
         name: 'Andika Setya Eka Natha',
         kelas: 'Kelas: 11 PPLG 1 || ',
         absen: "Absen: 07",
         image: 'image/pp.jpeg'),
-
-        User(
+    User(
         name: 'Antariksa Kusuma Hermawan',
         kelas: 'Kelas: 11 PPLG 1 || ',
         absen: "Absen: 08",
         image: 'image/pp.jpeg'),
-
-        User(
+    User(
         name: 'Azzra Rienov Fahlivi',
         kelas: 'Kelas: 11 PPLG 1 || ',
         absen: "Absen: 09",
         image: 'image/pp.jpeg'),
-
-        User(
+    User(
         name: 'Bayu Septian Kurniawan',
         kelas: 'Kelas: 11 PPLG 1 || ',
         absen: "Absen: 10",
         image: 'image/profil.jpeg'),
-
-        User(
+    User(
         name: 'Bhre Nabil Faeyza',
         kelas: 'Kelas: 11 PPLG 1 || ',
         absen: "Absen: 11",
         image: 'image/user.jpeg'),
-
-        User(
+    User(
         name: 'Bimo Adi Bramantyo',
         kelas: 'Kelas: 11 PPLG 1 || ',
         absen: "Absen: 12",
         image: 'image/user.jpeg'),
-
-        User(
+    User(
         name: 'Daffa Syauqi Syarif',
         kelas: 'Kelas: 11 PPLG 1 || ',
         absen: "Absen: 13",
         image: 'image/user.jpeg'),
-
-        User(
+    User(
         name: 'Danar Gading',
         kelas: 'Kelas: 11 PPLG 1 || ',
         absen: "Absen: 14",
         image: 'image/user.jpeg'),
-
-        User(
+    User(
         name: 'Dimas Bagus Adityas',
         kelas: 'Kelas: 11 PPLG 1 || ',
         absen: "Absen: 15",
         image: 'image/user.jpeg'),
-
-        User(
+    User(
         name: 'Firdaus Hafidz Al-Kaff',
         kelas: 'Kelas: 11 PPLG 1 || ',
         absen: "Absen: 16",
         image: 'image/user.jpeg'),
-
-        User(
+    User(
         name: 'Hegel Al-Rafli',
         kelas: 'Kelas: 11 PPLG 1 || ',
         absen: "Absen: 17",
         image: 'image/user.jpeg'),
-
-        User(
+    User(
         name: 'Hibatullah Fawwaz Hana',
         kelas: 'Kelas: 11 PPLG 1 || ',
         absen: "Absen: 18",
         image: 'image/user.jpeg'),
-
-        User(
+    User(
         name: 'Javier Gavra Abhinaya',
         kelas: 'Kelas: 11 PPLG 1 || ',
         absen: "Absen: 19",
         image: 'image/user.jpeg'),
-
-        User(
+    User(
         name: 'Khoiru Rizal Kalam Ismail',
         kelas: 'Kelas: 11 PPLG 1 || ',
         absen: "Absen: 20",
         image: 'image/user.jpeg'),
-
-        User(
+    User(
         name: 'Lugas Richtigo Kalam Hetami',
         kelas: 'Kelas: 11 PPLG 1 || ',
         absen: "Absen: 21",
         image: 'image/user.jpeg'),
-
-        User(
+    User(
         name: 'Meyssa Aqila Adikara',
         kelas: 'Kelas: 11 PPLG 1 || ',
         absen: "Absen: 22",
         image: 'image/user.jpeg'),
-
-        User(
+    User(
         name: 'Mikhail Haqeen',
         kelas: 'Kelas: 11 PPLG 1 || ',
         absen: "Absen: 23",
         image: 'image/user.jpeg'),
-
-        User(
+    User(
         name: 'Muh Wahyu Ageng Pambudi',
         kelas: 'Kelas: 11 PPLG 1 || ',
         absen: "Absen: 24",
         image: 'image/user.jpeg'),
-
-        User(
+    User(
         name: 'Muhammad Firdan Azhari',
         kelas: 'Kelas: 11 PPLG 1 || ',
         absen: "Absen: 25",
         image: 'image/user.jpeg'),
-
-        User(
+    User(
         name: 'Muhammad Nur Wahid Bimawan',
         kelas: 'Kelas: 11 PPLG 1 || ',
         absen: "Absen: 26",
         image: 'image/user.jpeg'),
-
-        User(
+    User(
         name: 'Muhammad Vitto Corleone',
         kelas: 'Kelas: 11 PPLG 1 || ',
         absen: "Absen: 27",
         image: 'image/user.jpeg'),
-
-        User(
+    User(
         name: 'Muhammad Zumar Ramadhan',
         kelas: 'Kelas: 11 PPLG 1 || ',
         absen: "Absen: 28",
         image: 'image/user.jpeg'),
-
-        User(
+    User(
         name: 'Nabil Rajankanz',
         kelas: 'Kelas: 11 PPLG 1 || ',
         absen: "Absen: 29",
         image: 'image/user.jpeg'),
-
-        User(
+    User(
         name: 'Nadhif Ariq Danendra',
         kelas: 'Kelas: 11 PPLG 1 || ',
         absen: "Absen: 30",
         image: 'image/user.jpeg'),
-
-        User(
+    User(
         name: 'Riko Aditya Zaki',
         kelas: 'Kelas: 11 PPLG 1 || ',
         absen: "Absen: 31",
         image: 'image/user.jpeg'),
-
-        User(
+    User(
         name: 'Rizqi Agung Pradana',
         kelas: 'Kelas: 11 PPLG 1 || ',
         absen: "Absen: 32",
         image: 'image/user.jpeg'),
-
-        User(
+    User(
         name: 'Sinatrya Rasyid Fawwaz',
         kelas: 'Kelas: 11 PPLG 1 || ',
         absen: "Absen: 33",
         image: 'image/user.jpeg'),
-
-        User(
+    User(
         name: 'Wulan Febrianti',
         kelas: 'Kelas: 11 PPLG 1 || ',
         absen: "Absen: 34",
@@ -217,9 +184,6 @@ class _ListSiswaState extends State<ListSiswa> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('List Siswa 11 PPLG 1'),
-      ),
       body: ListView.builder(
           padding: EdgeInsets.only(bottom: 20),
           itemCount: users.length,
@@ -229,7 +193,30 @@ class _ListSiswaState extends State<ListSiswa> {
               child: Padding(
                 padding: EdgeInsets.all(0.0),
                 child: ListTile(
-                  onTap: null,
+                  onLongPress: () {
+                    showDialog<String>(
+                      context: context,
+                      builder: (BuildContext context) => AlertDialog(
+                        title: const Text('Hapus Item'),
+                        content: const Text('Tekan Ok untuk Menghapus'),
+                        actions: <Widget>[
+                          TextButton(
+                            onPressed: () => Navigator.pop(context, 'Cancel'),
+                            child: const Text('Cancel'),
+                          ),
+                          TextButton(
+                            onPressed: () {
+                              Navigator.pop(context, 'ok');
+                              setState(() {
+                                users.remove(list);
+                              });
+                            },
+                            child: const Text('OK'),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
                   leading: CircleAvatar(
                     foregroundImage: AssetImage(list.image),
                   ),
--- belajar_listview/lib/main.dart
+++ belajar_listview/lib/main.dart
@@ -10,11 +10,73 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Data Siswa 11 PPLG 1',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      home: TabBarDemo(),
+    );
+  }
+}
+
+class TabBarDemo extends StatefulWidget {
+  @override
+  _TabBarDemoState createState() => _TabBarDemoState();
+}
+
+class _TabBarDemoState extends State<TabBarDemo>
+    with SingleTickerProviderStateMixin {
+  late TabController _controller;
+  int _selectedIndex = 0;
+
+  List<Widget> list = [
+    Tab(icon: Text('Chat')),
+    Tab(icon: Text('Status')),
+    Tab(icon: Text('Panggilan')),
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = TabController(length: list.length, vsync: this);
+
+    _controller.addListener(() {
+      setState(() {
+        _selectedIndex = _controller.index;
+      });
+      print("Selected Index: " + _controller.index.toString());
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          bottom: TabBar(
+            onTap: (index) {
+              // Should not used it as it only called when tab options are clicked,
+              // not when user swapped
+            },
+            controller: _controller,
+            tabs: list,
+          ),
+          title: Text('List Daftar Siswa'),
+        ),
+        body: TabBarView(
+          controller: _controller,
+          children: [
+            ListSiswa(),
+            Center(
+                child: Text(
+              _selectedIndex.toString(),
+              style: TextStyle(fontSize: 40),
+            )),
+            Center(
+                child: Text(
+              _selectedIndex.toString(),
+              style: TextStyle(fontSize: 40),
+            )),
+          ],
+        ),
       ),
-      home: ListSiswa(),
     );
   }
 }
